7e3f6e2fb2b34a439c2603d257de7d3ef90bbdf0
isaacdev07 <isaacsantos7953@gmail.com>
2025-11-07 17:42:27 -0300

    timeout 15000

--- senai-skill-up/src/services/api.js
+++ senai-skill-up/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const API_CONFIG = {
   BASE_URL: "https://tccdrakes.azurewebsites.net",
-  TIMEOUT: 10000, // Configurações de retry podem ser removidas se não estiverem sendo usadas // RETRY_ATTEMPTS: 3, // RETRY_DELAY: 1000,
+  TIMEOUT: 15000, // Configurações de retry podem ser removidas se não estiverem sendo usadas // RETRY_ATTEMPTS: 3, // RETRY_DELAY: 1000,
 };
 
 // Criar instância do Axios
