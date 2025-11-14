321914b819c79a819c7bd384f4a8ad6bd8999109
isaacdev07 <isaacsantos7953@gmail.com>
2025-11-14 19:08:22 -0300

    header e footer ok

--- senai-skill-up/src/hooks/useAuth.js
+++ senai-skill-up/src/hooks/useAuth.js
@@ -175,7 +175,7 @@ export const AuthProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider value={value}>
-            {children}   {" "}
+      {children}
     </AuthContext.Provider>
   );
 };
